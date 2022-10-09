d43aad143b8abc831677abd2b915cbac54e1b6f8
Vinicio <59233562+vinicioeuf@users.noreply.github.com>
2022-10-09 10:57:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,15 @@
  <head>
  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8237094830527218"
      crossorigin="anonymous"></script>
+ <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FYVCGF8EQT"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FYVCGF8EQT');
+</script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">   
     <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
