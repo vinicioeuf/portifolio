d2a6b8ab704d96578f0feaf8896b9f1238f2ecc3
Vinicio Eufrazio <vinicio.eufrazio@aluno.ifsertao-pe.edu.br>
2024-02-09 17:38:13 -0300

    Update validaForm.php

--- validaForm.php
+++ validaForm.php
@@ -1,25 +1,22 @@
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <?php
 require('conexao.php');
-$con = Conexao::getInstance();
 if(isset($_POST['submit'])){
+    $con = Conexao::getInstance();
     $tipoProjeto = $_POST['select'];
     $nomeProjeto = $_POST['nome'];
     $emailDoDono = $_POST['email'];
     $descricao = $_POST['detalhes'];
 
-    $inserirNoBanco = "INSERT INTO propostas (tipoProjeto, nomeProjeto, emailDoDono, descricao) VALUES ('$tipoProjeto', '$nomeProjeto', '$emailDoDono', '$descricao')";
+    $inserirNoBanco = "INSERT INTO propostas (tipoprojeto, nomeprojeto, emaildodono, descricao) VALUES ('$tipoProjeto', '$nomeProjeto', '$emailDoDono', '$descricao')";
     $stmt = $con->query($inserirNoBanco);
-    echo "<div class='toast' role='alert' aria-live='assertive' aria-atomic='true'>
-  <div class='toast-header'>
-    <img src='...' class='rounded me-2' alt='...'>
-    <strong class='me-auto'>Bootstrap</strong>
-    <small>11 mins ago</small>
-    <button type='button' class='btn-close' data-bs-dismiss='toast' aria-label='Close'></button>
-  </div>
-  <div class='toast-body'>
-    Proposta enviada!
-  </div>
-</div>"; 
+    header("Location: orcamento.php");
+    echo "<script>swal({
+        text: 'Solicitação feita! Aguarde uma resposta no seu e-mail.',
+        icon: 'success',
+    }).then((result) => {
+        window.location.href='orcamento.php';
+      });</script>"; 
 }
 
 ?>
